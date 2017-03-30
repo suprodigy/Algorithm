6fb29308bd8167e1785aafee6fbafeacdd2fd925
suprodigy <suprodigy@gmail.com>
2017-03-31 00:23:57 +0900

    Mar. 30, 2017

--- codeground/src/postfix/Main.java
+++ codeground/src/postfix/Main.java
@@ -4,9 +4,42 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 import java.util.Stack;
 
 public class Main {
+	
+	public static List<String> split(String expression) {	
+		expression.trim();
+		List<String> exps = new ArrayList<>();
+		
+		// 선형 탐색하면서 연산 최소 단위로 나눠서 저장
+		String temp = "";
+		for (int i=0; i<expression.length(); i++) {
+			char ch = expression.charAt(i);
+			switch (ch) {
+			case '+': case '-': case '/': case '*': case '^': 
+			case '(': case ')': case '{': case '}':
+				if (!temp.equals("")) {
+					exps.add(temp);
+					temp = "";
+				}
+				exps.add(String.valueOf(ch));
+				break;
+			case ' ':
+				break;
+			default:
+				temp += ch;
+			}
+		}
+		
+		if (!temp.equals("")) {
+			exps.add(temp);
+		}
+		
+		return exps;
+	}
+	
 	public static List<String> toPostfix(List<String> exps) throws Exception {
 		List<String> ret = new ArrayList<>();
 		
@@ -88,6 +121,16 @@ public class Main {
 	}
 	
 	public static void main(String[] args) throws Exception {
+//		Scanner sc = new Scanner(new FileInputStream("input.txt"));
+		Scanner sc = new Scanner(System.in);
 		
+		String str = sc.next();
+		List<String> list1 = split(str);
+		List<String> list2 = toPostfix(list1);
+		
+		for (String temp : list2) {
+			System.out.print(temp);
+		}
+		System.out.println();
 	}
 }
